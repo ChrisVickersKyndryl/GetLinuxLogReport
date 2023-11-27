f9cfd90e1cd6de67e1d788add1da8baed7725e18
ChrisVickersKyndryl <100736659+ChrisVickersKyndryl@users.noreply.github.com>
2023-11-27 14:52:23 +1100

    Update Get_Log_Size.sh

--- scripts/Get_Log_Size.sh
+++ scripts/Get_Log_Size.sh
@@ -2,21 +2,21 @@
 
 # Get daily value of logs created (uncompressed)
 if grep -q -E '^\s*?weekly$' /etc/logrotate.conf; then
-    FILESIZE=$(stat -c%s "$1")
+    FILESIZE=$(stat -c%s "{{ item }}")
     echo "$((FILESIZE/7))"
     exit
 fi
 
 # Get the daily amount of logs if set to monthly
 if grep -q -E '^\s*?monthly$' /etc/logrotate.conf; then
-    FILESIZE=$(stat -c%s "$1")
+    FILESIZE=$(stat -c%s "{{ item }}")
     echo "$((FILESIZE/30))"
     exit
 fi
 
 # Get the daily amount of logs if set to daily rotation
 if grep -q -E '^\s*?daily$' /etc/logrotate.conf; then
-    FILESIZE=$(stat -c%s "$1")
+    FILESIZE=$(stat -c%s "{{ item }}")
     echo "$((FILESIZE))"
     exit
 fi
